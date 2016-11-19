509f63cc45384e140def7abd256af49bcfbaf67c
cmitash <cm1074@rutgers.edu.com>
2016-11-18 19:52:09 -0500

    initial commit

---
+++ pcl-tools/CMakeFiles/normal_estimation_omp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal_estimation_omp.dir/src/normal_estimation_omp.cpp.o"
+  "normal_estimation_omp.pdb"
+  "normal_estimation_omp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal_estimation_omp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/pcl-tools/CMakeFiles/pcdnormal2ply.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcdnormal2ply.dir/src/pcdnormal2ply.cpp.o"
+  "pcdnormal2ply.pdb"
+  "pcdnormal2ply"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcdnormal2ply.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
